intel/llvm

commit c12ce4215408ddf83c338a4797b05dd0084b8d06

[offload] Add floating-point support detection queries (#193233)
Add device info queries to detect support for half-, single-, and
double-precision floating-point formats.

For the AMDGPU, CUDA, and Host plugins, add the new queries alongside
the existing capability reporting without changing current behavior.

For the Level Zero plugin, implement floating-point support detection
and capability querying.

offload/plugins-nextgen/amdgpu/src/rtl.cpp
@@ -31,6 +31,7 @@
 #include "Utils/ELF.h"
 
 #include "GlobalHandler.h"
+#include "OffloadAPI.h"
 #include "OpenMP/OMPT/Callback.h"
 #include "PluginInterface.h"
 #include "UtilitiesRTL.h"
@@ -3320,6 +3321,28 @@ struct AMDGPUDeviceTy : public GenericDeviceTy, AMDGenericDeviceTy {
     // Silently consume the error.
     if (Err)
       consumeError(std::move(Err));
+
+    ol_device_fp_capability_flags_t FPFlags =
+        OL_DEVICE_FP_CAPABILITY_FLAG_CORRECTLY_ROUNDED_DIVIDE_SQRT |
+        OL_DEVICE_FP_CAPABILITY_FLAG_ROUND_TO_NEAREST |
+        OL_DEVICE_FP_CAPABILITY_FLAG_ROUND_TO_ZERO |
+        OL_DEVICE_FP_CAPABILITY_FLAG_ROUND_TO_INF |
+        OL_DEVICE_FP_CAPABILITY_FLAG_INF_NAN |
+        OL_DEVICE_FP_CAPABILITY_FLAG_DENORM | OL_DEVICE_FP_CAPABILITY_FLAG_FMA;
+
+    Info.add("Single FP Support", true, "", DeviceInfo::SINGLE_FP_SUPPORT);
+    Info.add("Single FP Capabilities", FPFlags, "",
+             DeviceInfo::SINGLE_FP_CONFIG);
+
+    Info.add("Double FP Support", true, "", DeviceInfo::DOUBLE_FP_SUPPORT);
+    Info.add("Double FP Capabilities", FPFlags, "",
+             DeviceInfo::DOUBLE_FP_CONFIG);
+
+    // TODO: Use HSA_AGENT_INFO_FAST_F16_OPERATION to detect FP16 support.
+    Info.add("Half FP Support", ol_bool_t(false), "",
+             DeviceInfo::HALF_FP_SUPPORT);
+    Info.add("Half FP Capabilities", ol_device_fp_capability_flags_t{0}, "",
+             DeviceInfo::HALF_FP_CONFIG);
 
     return Info;
   }

offload/plugins-nextgen/cuda/src/rtl.cpp
@@ -21,6 +21,7 @@
 #include "Shared/Environment.h"
 
 #include "GlobalHandler.h"
+#include "OffloadAPI.h"
 #include "OpenMP/OMPT/Callback.h"
 #include "PluginInterface.h"
 #include "Utils/ELF.h"
@@ -1240,6 +1241,26 @@ struct CUDADeviceTy : public GenericDeviceTy {
       Info.add("Multi-Device Boars", bool(TmpInt));
 
     Info.add("Compute Capabilities", ComputeCapability.str());
+
+    ol_device_fp_capability_flags_t FPFlags =
+        OL_DEVICE_FP_CAPABILITY_FLAG_CORRECTLY_ROUNDED_DIVIDE_SQRT |
+        OL_DEVICE_FP_CAPABILITY_FLAG_ROUND_TO_NEAREST |
+        OL_DEVICE_FP_CAPABILITY_FLAG_ROUND_TO_ZERO |
+        OL_DEVICE_FP_CAPABILITY_FLAG_ROUND_TO_INF |
+        OL_DEVICE_FP_CAPABILITY_FLAG_INF_NAN |
+        OL_DEVICE_FP_CAPABILITY_FLAG_DENORM | OL_DEVICE_FP_CAPABILITY_FLAG_FMA;
+
+    Info.add("Single FP Support", true, "", DeviceInfo::SINGLE_FP_SUPPORT);
+    Info.add("Single FP Capabilities", FPFlags, "",
+             DeviceInfo::SINGLE_FP_CONFIG);
+
+    Info.add("Double FP Support", true, "", DeviceInfo::DOUBLE_FP_SUPPORT);
+    Info.add("Double FP Capabilities", FPFlags, "",
+             DeviceInfo::DOUBLE_FP_CONFIG);
+
+    Info.add("Half FP Support", false, "", DeviceInfo::HALF_FP_SUPPORT);
+    Info.add("Half FP Capabilities", ol_device_fp_capability_flags_t{0}, "",
+             DeviceInfo::HALF_FP_CONFIG);
 
     return Info;
   }

offload/plugins-nextgen/host/src/rtl.cpp
@@ -20,6 +20,7 @@
 #include "Utils/ELF.h"
 
 #include "GlobalHandler.h"
+#include "OffloadAPI.h"
 #include "OpenMP/OMPT/Callback.h"
 #include "PluginInterface.h"
 #include "omptarget.h"
@@ -403,6 +404,27 @@ struct GenELF64DeviceTy : public GenericDeviceTy {
     MaxSize.add("x", uint32_max);
     MaxSize.add("y", uint32_max);
     MaxSize.add("z", uint32_max);
+
+    ol_device_fp_capability_flags_t FPFlags =
+        OL_DEVICE_FP_CAPABILITY_FLAG_CORRECTLY_ROUNDED_DIVIDE_SQRT |
+        OL_DEVICE_FP_CAPABILITY_FLAG_ROUND_TO_NEAREST |
+        OL_DEVICE_FP_CAPABILITY_FLAG_ROUND_TO_ZERO |
+        OL_DEVICE_FP_CAPABILITY_FLAG_ROUND_TO_INF |
+        OL_DEVICE_FP_CAPABILITY_FLAG_INF_NAN |
+        OL_DEVICE_FP_CAPABILITY_FLAG_DENORM | OL_DEVICE_FP_CAPABILITY_FLAG_FMA;
+
+    Info.add("Single FP Support", true, "", DeviceInfo::SINGLE_FP_SUPPORT);
+    Info.add("Single FP Capabilities", FPFlags, "",
+             DeviceInfo::SINGLE_FP_CONFIG);
+
+    Info.add("Double FP Support", true, "", DeviceInfo::DOUBLE_FP_SUPPORT);
+    Info.add("Double FP Capabilities", FPFlags, "",
+             DeviceInfo::DOUBLE_FP_CONFIG);
+
+    Info.add("Half FP Support", false, "", DeviceInfo::HALF_FP_SUPPORT);
+    Info.add("Half FP Capabilities", ol_device_fp_capability_flags_t{0}, "",
+             DeviceInfo::HALF_FP_CONFIG);
+
     return Info;
   }
 

offload/liboffload/API/Device.td
@@ -47,7 +47,12 @@ def ol_device_info_t : Enum {
   ];
   list<TaggedEtor> fp_configs = !foreach(type, ["Single", "Double", "Half"], TaggedEtor<type # "_FP_CONFIG", "ol_device_fp_capability_flags_t", type # " precision floating point capability">);
   list<TaggedEtor> native_vec_widths = !foreach(type, ["char","short","int","long","float","double","half"], TaggedEtor<"NATIVE_VECTOR_WIDTH_" # type, "uint32_t", "Native vector width for " # type>);
-  let etors = !listconcat(basic_etors, fp_configs, native_vec_widths);
+  list<TaggedEtor> fp_support =
+      !foreach(type, ["Single", "Double", "Half"],
+               TaggedEtor<type#"_FP_SUPPORT", "bool",
+                          type#" precision floating point support">);
+  let etors =
+      !listconcat(basic_etors, fp_configs, native_vec_widths, fp_support);
 }
 
 def ol_device_fp_capability_flag_t : Enum {

offload/liboffload/src/OffloadImpl.cpp
@@ -429,22 +429,6 @@ Error olGetDeviceInfoImplDetail(ol_device_handle_t Device,
     else
       return Info.write<ol_device_type_t>(OL_DEVICE_TYPE_GPU);
 
-  case OL_DEVICE_INFO_SINGLE_FP_CONFIG:
-  case OL_DEVICE_INFO_DOUBLE_FP_CONFIG: {
-    ol_device_fp_capability_flags_t flags{0};
-    flags |= OL_DEVICE_FP_CAPABILITY_FLAG_CORRECTLY_ROUNDED_DIVIDE_SQRT |
-             OL_DEVICE_FP_CAPABILITY_FLAG_ROUND_TO_NEAREST |
-             OL_DEVICE_FP_CAPABILITY_FLAG_ROUND_TO_ZERO |
-             OL_DEVICE_FP_CAPABILITY_FLAG_ROUND_TO_INF |
-             OL_DEVICE_FP_CAPABILITY_FLAG_INF_NAN |
-             OL_DEVICE_FP_CAPABILITY_FLAG_DENORM |
-             OL_DEVICE_FP_CAPABILITY_FLAG_FMA;
-    return Info.write(flags);
-  }
-
-  case OL_DEVICE_INFO_HALF_FP_CONFIG:
-    return Info.write<ol_device_fp_capability_flags_t>(0);
-
   case OL_DEVICE_INFO_NATIVE_VECTOR_WIDTH_CHAR:
   case OL_DEVICE_INFO_NATIVE_VECTOR_WIDTH_SHORT:
   case OL_DEVICE_INFO_NATIVE_VECTOR_WIDTH_INT:
@@ -503,6 +487,9 @@ Error olGetDeviceInfoImplDetail(ol_device_handle_t Device,
   case OL_DEVICE_INFO_NUM_COMPUTE_UNITS:
   case OL_DEVICE_INFO_ADDRESS_BITS:
   case OL_DEVICE_INFO_MAX_CLOCK_FREQUENCY:
+  case OL_DEVICE_INFO_SINGLE_FP_CONFIG:
+  case OL_DEVICE_INFO_DOUBLE_FP_CONFIG:
+  case OL_DEVICE_INFO_HALF_FP_CONFIG:
   case OL_DEVICE_INFO_MEMORY_CLOCK_RATE: {
     // Uint32 values
     if (!std::holds_alternative<uint64_t>(Entry->Value))
@@ -520,6 +507,16 @@ Error olGetDeviceInfoImplDetail(ol_device_handle_t Device,
       return makeError(ErrorCode::BACKEND_FAILURE,
                        "plugin returned incorrect type");
     return Info.write(std::get<uint64_t>(Entry->Value));
+  }
+
+  case OL_DEVICE_INFO_SINGLE_FP_SUPPORT:
+  case OL_DEVICE_INFO_DOUBLE_FP_SUPPORT:
+  case OL_DEVICE_INFO_HALF_FP_SUPPORT: {
+    // Boolean values
+    if (!std::holds_alternative<bool>(Entry->Value))
+      return makeError(ErrorCode::BACKEND_FAILURE,
+                       "plugin returned incorrect type");
+    return Info.write<bool>(std::get<bool>(Entry->Value));
   }
 
   case OL_DEVICE_INFO_MAX_WORK_SIZE_PER_DIMENSION:

offload/plugins-nextgen/common/CMakeLists.txt
@@ -53,6 +53,7 @@ target_link_options(PluginCommon PUBLIC ${offload_link_flags})
 target_include_directories(PluginCommon PUBLIC
   ${CMAKE_CURRENT_SOURCE_DIR}/include
   ${CMAKE_CURRENT_BINARY_DIR}/include
+  ${CMAKE_CURRENT_BINARY_DIR}/../../liboffload/API
   ${LIBOMPTARGET_LLVM_INCLUDE_DIRS}
   ${LIBOMPTARGET_BINARY_INCLUDE_DIR}
   ${LIBOMPTARGET_INCLUDE_DIR}

offload/plugins-nextgen/level_zero/dynamic_l0/L0DynWrapper.cpp
@@ -50,6 +50,7 @@ DLWRAP(zeDeviceCanAccessPeer, 3)
 DLWRAP(zeDeviceGetProperties, 2)
 DLWRAP(zeDeviceGetCommandQueueGroupProperties, 3)
 DLWRAP(zeDeviceGetComputeProperties, 2)
+DLWRAP(zeDeviceGetModuleProperties, 2)
 DLWRAP(zeDeviceGetMemoryProperties, 3)
 DLWRAP(zeDeviceGetCacheProperties, 3)
 DLWRAP(zeDeviceGetGlobalTimestamps, 3)

offload/plugins-nextgen/level_zero/dynamic_l0/level_zero/ze_api.h
@@ -148,6 +148,7 @@ typedef enum _ze_structure_type_t {
   ZE_STRUCTURE_TYPE_DRIVER_PROPERTIES = 0x1,
   ZE_STRUCTURE_TYPE_DEVICE_PROPERTIES = 0x3,
   ZE_STRUCTURE_TYPE_DEVICE_COMPUTE_PROPERTIES = 0x4,
+  ZE_STRUCTURE_TYPE_DEVICE_MODULE_PROPERTIES = 0x5,
   ZE_STRUCTURE_TYPE_COMMAND_QUEUE_GROUP_PROPERTIES = 0x6,
   ZE_STRUCTURE_TYPE_DEVICE_MEMORY_PROPERTIES = 0x7,
   ZE_STRUCTURE_TYPE_DEVICE_CACHE_PROPERTIES = 0x9,
@@ -399,6 +400,53 @@ typedef struct _ze_device_cache_properties_t {
   size_t cacheSize;
 } ze_device_cache_properties_t;
 
+/* Native kernel UUID */
+#ifndef ZE_MAX_NATIVE_KERNEL_UUID_SIZE
+#define ZE_MAX_NATIVE_KERNEL_UUID_SIZE 16
+#endif
+
+typedef struct _ze_native_kernel_uuid_t {
+  uint8_t id[ZE_MAX_NATIVE_KERNEL_UUID_SIZE];
+} ze_native_kernel_uuid_t;
+
+/* Device module flags */
+typedef uint32_t ze_device_module_flags_t;
+typedef enum _ze_device_module_flag_t {
+  ZE_DEVICE_MODULE_FLAG_FP16 = ZE_BIT(0),
+  ZE_DEVICE_MODULE_FLAG_FP64 = ZE_BIT(1),
+  ZE_DEVICE_MODULE_FLAG_INT64_ATOMICS = ZE_BIT(2),
+  ZE_DEVICE_MODULE_FLAG_DP4A = ZE_BIT(3),
+  ZE_DEVICE_MODULE_FLAG_FORCE_UINT32 = 0x7fffffff
+} ze_device_module_flag_t;
+
+/* Floating-point capability flags */
+typedef uint32_t ze_device_fp_flags_t;
+typedef enum _ze_device_fp_flag_t {
+  ZE_DEVICE_FP_FLAG_DENORM = ZE_BIT(0),
+  ZE_DEVICE_FP_FLAG_INF_NAN = ZE_BIT(1),
+  ZE_DEVICE_FP_FLAG_ROUND_TO_NEAREST = ZE_BIT(2),
+  ZE_DEVICE_FP_FLAG_ROUND_TO_ZERO = ZE_BIT(3),
+  ZE_DEVICE_FP_FLAG_ROUND_TO_INF = ZE_BIT(4),
+  ZE_DEVICE_FP_FLAG_FMA = ZE_BIT(5),
+  ZE_DEVICE_FP_FLAG_ROUNDED_DIVIDE_SQRT = ZE_BIT(6),
+  ZE_DEVICE_FP_FLAG_SOFT_FLOAT = ZE_BIT(7),
+  ZE_DEVICE_FP_FLAG_FORCE_UINT32 = 0x7fffffff
+} ze_device_fp_flag_t;
+
+/* Device module properties */
+typedef struct _ze_device_module_properties_t {
+  ze_structure_type_t stype;
+  void *pNext;
+  uint32_t spirvVersionSupported;
+  ze_device_module_flags_t flags;
+  ze_device_fp_flags_t fp16flags;
+  ze_device_fp_flags_t fp32flags;
+  ze_device_fp_flags_t fp64flags;
+  uint32_t maxArgumentsSize;
+  uint32_t printfBufferSize;
+  ze_native_kernel_uuid_t nativeKernelSupported;
+} ze_device_module_properties_t;
+
 /* Device IP version (extension) */
 typedef struct _ze_device_ip_version_ext_t {
   ze_structure_type_t stype;
@@ -611,6 +659,9 @@ ZE_APIEXPORT ze_result_t ZE_APICALL zeDeviceGetProperties(
 ZE_APIEXPORT ze_result_t ZE_APICALL zeDeviceGetComputeProperties(
     ze_device_handle_t hDevice,
     ze_device_compute_properties_t *pComputeProperties);
+ZE_APIEXPORT ze_result_t ZE_APICALL
+zeDeviceGetModuleProperties(ze_device_handle_t hDevice,
+                            ze_device_module_properties_t *pModuleProperties);
 ZE_APIEXPORT ze_result_t ZE_APICALL
 zeDeviceGetMemoryProperties(ze_device_handle_t hDevice, uint32_t *pCount,
                             ze_device_memory_properties_t *pMemProperties);

lplewa/llvm-project

commit 37b9324df09b70d220396ba1c590f106739ff06c

[offload] fix compilation issue caused by #193233

offload/plugins-nextgen/amdgpu/src/rtl.cpp
@@ -3339,8 +3339,7 @@ struct AMDGPUDeviceTy : public GenericDeviceTy, AMDGenericDeviceTy {
              DeviceInfo::DOUBLE_FP_CONFIG);
 
     // TODO: Use HSA_AGENT_INFO_FAST_F16_OPERATION to detect FP16 support.
-    Info.add("Half FP Support", ol_bool_t(false), "",
-             DeviceInfo::HALF_FP_SUPPORT);
+    Info.add("Half FP Support", false, "", DeviceInfo::HALF_FP_SUPPORT);
     Info.add("Half FP Capabilities", ol_device_fp_capability_flags_t{0}, "",
              DeviceInfo::HALF_FP_CONFIG);
 